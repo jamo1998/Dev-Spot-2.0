d61d9ebdd7c18f232679146f9ea134a9b0749c8e
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-07-30 10:51:48 -0700

    add comments

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -3,7 +3,7 @@ const auth = require('../middleware/auth')
 const Project = require('../models/project')
 
 
-
+// route for posting comments on a specific project
 router.post('/:id/comments', (req, res) => {
     Project.findById(req.params.id, (err, project) => {
         project.comments.push(req.body);
@@ -12,56 +12,22 @@ router.post('/:id/comments', (req, res) => {
         });
     });
 })
-// route to post comments to projects
-// router.post('/new', async (req, res) => {
-//     Project.findById({
-//         projectId: ''
-//     })
-//     // find projectId (make string)
-//     // make call to it in database 
 
-//     console.log(`🇸🇭`)
-//     try {
-//         const { entry } = req.body
-        
-//         const newComment = new Comment({
-//             name: req.body.name,
-//             entry: req.body.entry
-//         })
-//         const savedComment = await newComment.save()
-//         res.json(savedComment)
-//     } catch (err) {
-//         res.status(500).json({ error: err.message})
+// // route for deleting specific comments on specific projects
+// router.delete('/:id/comments/:commentId', (req, res) => {
+//     let projectComments = Project.findById(req.params._id)
+//     console.log(projectComments)
+//     console.log(projectComments.comments)
+//     let commentIndex = Project.comments.findIndex(comments => comments._id == req.params.commentId)
+//     if(commentIndex === -1) {
+//         res.redirect('/projects')
+//     } else {
+//         Project.comments.splice(commentIndex, 1)
+//         Project.save(function (err) {
+//             if (err) return handleError(err);
+//             console.log('the comments were removed');
+//         });
 //     }
 // })
-
-router.delete('/:id/comments/:commentId', (req, res) => {
-    let commentIndex = Project.comments.findIndex(comment => comment._id == req.params.commentId)
-    if(commentIndex === -1) {
-        res.redirect('/projects')
-    } else {
-        Project.comments.splice(commentIndex, 1)
-        Project.save(function (err) {
-            if (err) return handleError(err);
-            console.log('the comments were removed');
-        });
-    }
-
-    // Project.findById(req.params.id, (err, project) => {
-    //     project.comments.pop(req.body)
-    //     project.save(function (err) {
-    //         res. redirect(`/project/${project._id}`)
-    //     })
-    // })
-
-    // const comment = await Comment.findOne({
-    //     _id: req.params._id
-    // })
-    // if(!comment){
-    //     return res.status(400).json({ msg: 'Cannot find the comment you want to delete'})
-    // }
-    // const deletedComment = await Comment.findByIdAndDelete(req.params.id)
-    // res.json(deletedComment)
-})
 
 module.exports = router
