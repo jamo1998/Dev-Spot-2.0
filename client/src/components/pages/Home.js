350fbfe97e2186031d26dbab9fe9ee370c41875b
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-07-30 12:11:47 -0500

    functioning home page axios call

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,70 @@
-import React, {useEffect, useContext} from 'react'
+import React, {useEffect, useContext, useState} from 'react';
 import {useHistory} from 'react-router-dom';
-import UserContext from '../../context/UserContext'
+import UserContext from '../../context/UserContext';
 import Axios from 'axios';
 
 export default function Home() {
   const {userData} = useContext(UserContext);
   const history = useHistory();
+  const [toggleRefresh, setToggleRefresh] = useState(false);
+  const [userInfo, setUserInfo] = useState({})
+
+  // async function getUserData( ) {
+  //   //semantic naming^
+  //   if(!userData.user) history.push('/login');
+  //   try {
+  //     const userInfo = await Axios.get('http://localhost:5000/users', {
+  //       headers:{
+  //         "x-auth-token":userData.token
+  //       }
+  //     });
+  //     setUserInfo(userInfo.data);
+  //         console.log(userInfo.data)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // useEffect(() => {getUserData()}, []);
 
   useEffect(() => {
-    if(!userData.user) history.push('/login')
-  });
+    Axios.get('http://localhost:5000/users', {
+      headers:{
+        "x-auth-token":userData.token
+      }
+    }).then(response => {
+      setUserInfo(response.data);
+    }).catch(err => {
+      console.log(err)
+    }, )
 
-  console.log(userData.user)
+  }, [])
+
+  // if(!userInfo.user) {
+  //   return null
+  // }
+
   return (
-    <div className="container">Home</div>
+    <div className="container">
+      <div className="card row">
+        <div className="center">
+            <p>Name: {userInfo.username} </p>
+            <p>Email: </p>
+            <p>Github: {userInfo.githubURL }</p>
+            <p>Portfolio: {userInfo.portfolioURL}</p>
+            </div>
+            <div className="center row">
+                <div className="col s8">
+                    <p>Bio: </p>
+                    <p>{userInfo.bio}</p>
+                </div>
+            </div>
+      </div> 
+      <div className="row">       
+        <div className="card center">
+            <p>Projects you have opened!</p>
+        </div>
+      </div>  
+    </div>
   )
 }
